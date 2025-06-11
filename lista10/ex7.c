57ba0c7dc4d0a879adcea0f2b97f1713da58a892
juan-ribeiro02 <juan.rib02@gmail.com>
2025-06-11 20:38:30 -0300

    mais

--- lista10/ex7.c
+++ lista10/ex7.c
@@ -6,25 +6,48 @@
 
 #include <stdio.h>
 
-void removeNumVetor(float vetor[], int tamanho, float numRemove);
+int removeNumVetor(float vetor[], int tamanho, float numRemove);
 
 void main()
 {
-
-}
-
-void removeNumVetor(float vetor[], int tamanho, float numRemove)
-{
-    int i;
+    int i, tamanho = 5, resul;
+    float vet[tamanho], num;
 
     for (i = 0; i < tamanho; i++)
     {
+        printf("Informe um numero:");
+        scanf("%f", &vet[i]);
+    }
+
+    printf("Informe outro numero:");
+    scanf("%f", &num);
+
+    resul = removeNumVetor(vet, tamanho, num);
+
+    printf("Numero de remocoes:%d", resul);
+
+    for(i = 0; i < tamanho - resul + 1; i++)
+    {
+        printf("%f\n", vet[i]);
+    }
+}
+
+int removeNumVetor(float vetor[], int tamanho, float numRemove)
+{
+    int i, j, remocoes = 0;
+
+    for (i = 0; i < tamanho - remocoes; i++)
+    {
         if (vetor[i] == numRemove)
         {
-            for (; i < tamanho; i++)
+            for (j = i; j < tamanho - remocoes - 1; j++)
             {
-                vetor[i] = vetor[i + 1];
+                vetor[j] = vetor[j + 1];
             }
+            remocoes++;
+            i--;
         }
     }
+
+    return remocoes;
 }
